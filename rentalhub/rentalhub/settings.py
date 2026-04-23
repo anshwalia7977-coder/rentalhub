1054c04e76d8c216a959099c0e861f1dbe368f83
anshwalia7977-coder <anshwalia7977@gmail.com>
2026-04-23 02:29:33 -0400

    Update settings.py

--- rentalhub/rentalhub/settings.py
+++ rentalhub/rentalhub/settings.py
@@ -129,8 +129,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'anshwalia7977@gmail.com'
-EMAIL_HOST_PASSWORD = 'ymam kpii zjjh vvwa'
+EMAIL_HOST_USER = 'sample@gmail.com'
+EMAIL_HOST_PASSWORD = 'ymam kpil gjjh wvwa'
  
 import os
 MEDIA_URL = '/media/'
